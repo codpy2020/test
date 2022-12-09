4171aab2a06c0eee26c3f82416a9e0f6e8068363
codpy2020 <67156845+codpy2020@users.noreply.github.com>
2022-12-09 11:25:04 +0100

    update

--- setup.py
+++ setup.py
@@ -38,6 +38,19 @@ long_description = open("README.md","r").read()
 
 #print("find_packages():",find_packages(),)
 
+
+
+cmdclass = {}
+ext_modules = []
+include_dirs = []
+
+
+# Build extension with previously Cython generated source.
+ext_modules.append(Extension("printf", ["codpy/printf.c"]))
+
+
+
+
 setup(
     name=DISTNAME,
     version=__version__,
@@ -61,7 +74,8 @@ setup(
     ],
     #install_requires=['SobolSequence==0.2.1','google-api-python-client==2.63.0','oauth2client==4.1.3','pybind11==2.10.0','pandas==1.5.0','tk','numpy==1.22.0','matplotlib==3.6.0','mkl','scikit-learn==1.1.3','scipy==1.9.2','tensorflow==2.10.0','seaborn==0.12.0', 'scikit-image==0.19.3','tensorflow-datasets==4.6.0','torch==1.12.1','xgboost==1.6.2','jupyter==1.0.0','quantlib==1.27','xlrd==2.0.1','pydicom==2.3.0','yfinance==0.1.74','xarray==2022.9.0','kaggle==1.5.12','pandera','statsmodels'],
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    ext_modules=ext_modules
   #  extras_require={
   #  'win32': 'pywin32'
   #}
